73f15807c4252e25e447cb1fd5b40fe9cbd490d0
Dmitry Dorokhov <b27011971@inbox.ru>
2024-01-22 18:41:42 +0300

    feat: create a project. Add dependencies [#1]

---
+++ src/main/java/com/example/charfreq/CharFreqApplication.java
@@ -0,0 +1,13 @@
+package com.example.charfreq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharFreqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharFreqApplication.class, args);
+	}
+
+}
