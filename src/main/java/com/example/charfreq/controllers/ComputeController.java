100ffbb336c72d9b47ca348f2e54c5ef59ea95c8
Dmitry Dorokhov <b27011971@inbox.ru>
2024-01-23 17:24:37 +0300

    [docs] add descriptions [#9]

--- src/main/java/com/example/charfreq/controllers/ComputeController.java
+++ src/main/java/com/example/charfreq/controllers/ComputeController.java
@@ -1,6 +1,9 @@
 package com.example.charfreq.controllers;
 
 import com.example.charfreq.services.ComputeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +14,7 @@ import java.util.Map;
 @Slf4j
 @RestController()
 @RequestMapping("/computes")
+@Tag(name = "Compute Controller", description = "API for character frequency computation")
 public class ComputeController {
 
     private final ComputeService computeService;
@@ -19,6 +23,10 @@ public class ComputeController {
         this.computeService = computeService;
     }
 
+    @Operation(summary = "Compute Character Frequency", description = "Calculates the frequency of characters in the provided input string.")
+    @ApiResponse(responseCode = "200", description = "Successful")
+    @ApiResponse(responseCode = "400", description = "Bad Request - Input string is empty")
+    @ApiResponse(responseCode = "413", description = "Payload Too Large")
     @PostMapping
     public ResponseEntity<Map<Character, Long>> compute(@RequestBody(required = false) String input) {
         return ResponseEntity
