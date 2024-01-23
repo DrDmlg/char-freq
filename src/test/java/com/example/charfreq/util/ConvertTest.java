249c307ca6d48de6f6c6d7f60047f3bdddc7b360
Dmitry Dorokhov <b27011971@inbox.ru>
2024-01-23 15:40:14 +0300

    test: test Convert class [#6]

---
+++ src/test/java/com/example/charfreq/util/ConvertTest.java
@@ -0,0 +1,21 @@
+package com.example.charfreq.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConvertTest {
+
+    @Test
+    void testConvertToMap_ShouldConvertLineToMap() {
+        String actual = "aaaaabcccc";
+
+        Map<Character, Long> expected = Convert.convertToMap(actual);
+
+        assertEquals(5, expected.get('a'));
+        assertEquals(4, expected.get('c'));
+        assertEquals(1, expected.get('b'));
+    }
+}
